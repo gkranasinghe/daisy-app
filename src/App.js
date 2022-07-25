b9594083cc6c91c0e5cf19cbabc354928637840e
gkranasinghe <gkranasinghe@gmail.com>
2022-07-25 10:42:50 +0530

    Photo Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import Intro from './components/Intro/Intro';
+import Navbar from './components/Navbar/Navbar';
+import Photo from './components/Photo/Photo';
 import './App.css';
 
 function App() {
   return (
-    <><Intro /></>
+    <>
+    <Navbar/>
+
+    <Intro />
+    <Photo/>
+    </>
   );
 }
 
